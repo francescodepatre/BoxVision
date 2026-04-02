15e332b1ea927158f1c66bacb105981f7bec7480
Francesco De Patre <fra.dp01@outlook.com>
2026-04-02 13:19:00 +0200

    Initial Commit

---
+++ BoxVision/BoxVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxVisionApp.swift
+//  BoxVision
+//
+//  Created by Francesco De Patre on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BoxVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoxVisionTests/BoxVisionTests.swift
@@ -0,0 +1,19 @@
+//
+//  BoxVisionTests.swift
+//  BoxVisionTests
+//
+//  Created by Francesco De Patre on 02/04/26.
+//
+
+import Testing
+@testable import BoxVision
+
+struct BoxVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
